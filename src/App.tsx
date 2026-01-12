622e9646d0f05a46a0ca23205942300941a861c5
Martinez Rodrigo Javier <rodripincha7@gmail.com>
2026-01-12 17:06:15 -0300

    Inicializando Frontend: React + Vite + TS Clean

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>CodeBase - Frontend</h1>
+      <p>Plataforma White Label inicializada.</p>
+    </div>
+  )
+}
+
+export default App
